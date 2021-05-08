d522cdb6c94b982fbc48d565aae65441c6beeb15
vanshika-99 <54948141+vanshika-99@users.noreply.github.com>
2021-05-08 19:13:26 -0400

    added token refresh and logout functionality

--- auth.js
+++ auth.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const accessTokenSecret = 'youraccesstokensecret';
 
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
+
 app.use(bodyParser.json());
 
 const users = [
@@ -24,20 +27,55 @@ app.listen(3000, () => {
 });
 
 app.post('/login', (req, res) => {
-    // Read username and password from request body
+    // read username and password from request body
     const { username, password } = req.body;
 
-    // Filter user from the users array by username and password
+    // filter user from the users array by username and password
     const user = users.find(u => { return u.username === username && u.password === password });
 
     if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
 
         res.json({
-            accessToken
+            accessToken,
+            refreshToken
         });
     } else {
         res.send('Username or password incorrect');
     }
+});
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    refreshTokens = refreshTokens.filter(token => t !== token);
+
+    res.send("Logout successful");
 });
